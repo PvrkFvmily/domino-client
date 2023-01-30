15d740f9cdc402c981f192e37c00417aa79dd33d
PvrkFvmily <wlsgnl9973@gmail.com>
2023-01-30 12:18:41 -0800

    lunch break working on circle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,13 @@ export default function Footer() {
         <div className="footer-container">
             <div className="footer">
                 {/* our names can be links to our personal page or linkedin */}
-                <p><strong>Team Domino</strong>: Daniel Park | Maria Hamilton | Josh Wu | Theo Robinson</p>
-                <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fed27de5c148a7bccd1509e_5847f98fcef1014c0b5e48c0.png" width="25" />
-                <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fecc7834dba831ccbeaa7fa_icons8-linkedin-2-512.png" width="25" />
+                <div>
+                    <p><strong>Team Domino</strong>: <span className="circle-d"></span>Daniel Park | <span className="circle-m"></span>Maria Hamilton | <span className="circle-j"></span>Josh Wu | <span className="circle-t"></span>Theo Robinson</p>
+                </div>
+                <div>
+                    <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fed27de5c148a7bccd1509e_5847f98fcef1014c0b5e48c0.png" width="20" />
+                    <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fecc7834dba831ccbeaa7fa_icons8-linkedin-2-512.png" width="20" />
+                </div>
             </div>
         </div>
     )
