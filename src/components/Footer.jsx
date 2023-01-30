91d5fef108495b8a31ecee5669520a7c2ec5251c
PvrkFvmily <wlsgnl9973@gmail.com>
2023-01-30 11:28:12 -0800

    starting on footer function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,8 @@ export default function Footer() {
             <div className="footer">
                 {/* our names can be links to our personal page or linkedin */}
                 <p><strong>Team Domino</strong>: Daniel Park | Maria Hamilton | Josh Wu | Theo Robinson</p>
+                <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fed27de5c148a7bccd1509e_5847f98fcef1014c0b5e48c0.png" width="25" />
+                <img src="https://assets.website-files.com/5fecc783f723ca5dd56bc92e/5fecc7834dba831ccbeaa7fa_icons8-linkedin-2-512.png" width="25" />
             </div>
         </div>
     )
